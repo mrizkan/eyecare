0a6a087eb51b367792644fa22f51cb806616c5db
Rizkan Mohamed <rizkan@itmartx.net>
2018-05-12 00:23:05 +0530

    download page done

---
+++ application/views/download.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rizkan
+ * Date: 5/4/2018
+ * Time: 8:44 PM
+ */
+?>
+<?php include ('inc/header.php'); ?>
+
+<!-- Start Banner -->
+
+<div class="inner-banner blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="content">
+                    <h1>Download Section</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Banner -->
+
+<!-- Start News & Events -->
+
+
+
+<section class="news-events padding-lg">
+    <div class="container">
+
+        <ul class="row cs-style-3">
+
+                <li class="col-sm-4" style="margin-bottom: 20px;">
+                    <div class="inner">
+                        <figure><img src="<?= base_url() ?>media/images/logo.png" class="img-responsive">
+                            <figcaption>
+                                <div class="cnt-block"><a href="" class="plus-icon">+</a>
+                                    <h3>Download the Document<br><br></h3>
+
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </li>
+
+
+
+        </ul>
+
+    </div>
+    </div>
+</section>
+<!-- End News & Events -->
+
+
+
+
+
+
+<?php include ('inc/footer.php'); ?>
